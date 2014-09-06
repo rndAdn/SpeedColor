7ddc2a355ac9784b15374acb53907721e821005a
Adequin Renaud <adequin.renaud@gmail.com>
2014-09-06 19:26:39 +0200

    First commit

---
+++ android/src/com/speedColor/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.speedColor.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.speedColor.game.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
